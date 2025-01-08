b4b20efbc87615a7d8ba678f79502aeccf665284
agosquevedo <agosqdo@gmail.com>
2025-01-08 10:43:06 -0300

    first commit

---
+++ static/script.js
@@ -0,0 +1,4 @@
+document.getElementById("playlist-button").addEventListener("click", () => {
+    window.open("https://music.youtube.com/playlist?list=PL5VrzlNQ9V2RCZSdrd5ehtRpYlkT7DdPf&si=9nUtgBhfP44sSO_U", "_blank");
+  });
+  
